a46fc4ae6f83ab87a32c7670c0ce7d9c6e9c3e44
subbotin.59 <gsubbotin.2019@gmail.com>
2024-05-27 12:51:24 +0500

    Созданный класс Config.Graf

---
+++ Classes/Database/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager_Graf.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=192.168.0.111;" +
+            "uid=root;" +
+            "pwd=root;" +
+            "database=TaskManager;";
+
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
