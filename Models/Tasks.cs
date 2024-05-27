81466af727b8dbc0f11f78a3331b71d861160669
subbotin.59 <gsubbotin.2019@gmail.com>
2024-05-27 13:11:53 +0500

    LastSave.Graf

---
+++ Models/Tasks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using TaskManager_Graf.Classes;
+using Schema = System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager_Graf.Models
+{
+    public class Tasks : Notification
+    {
+        public int Id { get; set; }
+    }
+}
