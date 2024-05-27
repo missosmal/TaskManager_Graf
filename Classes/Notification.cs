61c68116f02dc9c3dd4afc131c52a7e363acb685
subbotin.59 <gsubbotin.2019@gmail.com>
2024-05-27 13:02:43 +0500

    Разработанный классы общего-пользования.Graf

---
+++ Classes/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TaskManager_Graf.Classes
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
